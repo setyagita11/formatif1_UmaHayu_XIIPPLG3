f75209986367bbc72af059fe04082f7786e35164
setyagita11 <umafagansa@gmail.com>
2023-07-27 13:05:55 +0700

    ...

--- app/src/main/java/com/zubet/formatif1_umahayu_xiipplg3/MainActivity.kt
+++ app/src/main/java/com/zubet/formatif1_umahayu_xiipplg3/MainActivity.kt
@@ -4,17 +4,27 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import com.zubet.formatif1_umahayu_xiipplg3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // 1. val latenit binding
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // HAPUS "setContentView(R.layout.activity_main)"
 
-        //Variabel
-        val fab = findViewById<View>(R.id.floatingActionButton)
-        //Perintah
-        fab.setOnClickListener{
-            startActivity(Intent(applicationContext, input_barang::class.java))
+        // 2. inflate binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        // 3. setContentView (spy objectnya keluar)
+        setContentView(binding.root)
+        binding.floatingActionButton.setOnClickListener{
+            startActivity(
+                Intent(this,input_barang::class.java)
+            )
         }
+
     }
 }
